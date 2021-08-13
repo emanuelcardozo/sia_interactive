b2fa12058ec1a26069d877580c92f05a0e292e88
Emanuel Cardozo <leuname.01@gmail.com>
2021-08-13 12:38:48 -0300

    Making first Websocket connection

--- app/server.js
+++ app/server.js
@@ -1,11 +1,17 @@
 const path = require('path')
 const app = require('express')()
-const port = 3000
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const port = 3001;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
-app.listen(port, function(){
+io.on('connection', function() {
+    console.log('new Connection')
+})
+
+server.listen(port, function(){
     console.log('Server listening on port', port)
 })
